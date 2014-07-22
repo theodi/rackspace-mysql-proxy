4af8f8f56af70c0f7c55fc55592cd4bc91eb1e68
pikesley <github@orgraphone.org>
2014-07-22 11:24:56 +0100

    TDI is amaze

--- test/integration/default/serverspec/localhost/database_spec.rb
+++ test/integration/default/serverspec/localhost/database_spec.rb
@@ -3,3 +3,8 @@ require 'spec_helper'
 describe service('mysql') do
   it { should be_running }
 end
+
+describe command('mysql -h localhost -u root -pilikerandompasswords -e "show databases"') do
+  it { should return_exit_status 0 }
+  it { should return_stdout /Database/}
+end
