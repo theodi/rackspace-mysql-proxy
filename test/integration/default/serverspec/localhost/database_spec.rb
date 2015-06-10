e140de8e290610d7285f69977d4b771d0f1e082c
pikesley <github@orgraphone.org>
2015-06-10 14:16:23 +0100

    For new server spec

--- test/integration/default/serverspec/localhost/database_spec.rb
+++ test/integration/default/serverspec/localhost/database_spec.rb
@@ -1,14 +1,15 @@
-require 'spec_helper'
+require 'serverspec'
+set :backend, :exec
 
 describe service('mysql') do
   it { should be_running }
 end
 
 describe command('mysql -h localhost -u root -pilikerandompasswords -e "show databases"') do
-  it { should return_exit_status 0 }
-  it { should return_stdout /Database/}
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /Database/}
 end
 
 describe command('mysql -h localhost -u certificate -pthisisafakepassword certificate -e "show tables"') do
-  it { should return_exit_status 0 }
+  its(:exit_status) { should eq 0 }
 end
